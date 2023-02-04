4cc9dc8bb8b7b605a5f1b21fc1ba5a819fb879ab
Christopher Head <chead@chead.ca>
2023-02-04 14:47:24 -0800

    Move player gun controls to player.gd

--- game/player/gun.gd
+++ game/player/gun.gd
@@ -12,26 +12,10 @@ var projectile_count := 1
 @export_range(0, 9)
 var accuracy := 0
 
-## Whether the mouse pointer was moved recently, where “recently” means more recently than the last
-## time the aiming joystick was deflected above its dead zone
-var mouse_recent := true
-
 ## The root player node
 @onready
 var player := get_parent()
 
-func _unhandled_input(event: InputEvent) -> void:
-	var mouse := event as InputEventMouseMotion
-	if mouse != null:
-		mouse_recent = true
-	var pad_aim := Input.get_vector("aim_left", "aim_right", "aim_up", "aim_down")
-	if not pad_aim.is_zero_approx():
-		mouse_recent = false
-		rotation = pad_aim.angle()
-
-func _physics_process(_delta: float) -> void:
-	if mouse_recent:
-		rotation = player.get_local_mouse_position().angle()
 
 func _fire() -> void:
 	var angle_range := (45 - 5 * accuracy) / 360.0 * TAU
--- game/player/player.gd
+++ game/player/player.gd
@@ -21,5 +21,29 @@ var force_multiplier: float
 @export_range(0, 10) var scream_charge_maximum_level: int = 0
 @export_range(0, 10) var health_regen_level: int = 0
 
+## Whether the mouse pointer was moved recently, where “recently” means more recently than the last
+## time the aiming joystick was deflected above its dead zone
+var _mouse_recent := true
+
+## The player’s gun
+@onready
+var gun := $Gun
+
+
 func _unhandled_input(event: InputEvent) -> void:
+	# Handle movement
 	constant_force = Input.get_vector("left", "right", "up", "down") * force_multiplier
+
+	# Handle aiming
+	var mouse := event as InputEventMouseMotion
+	if mouse != null:
+		_mouse_recent = true
+	var pad_aim := Input.get_vector("aim_left", "aim_right", "aim_up", "aim_down")
+	if not pad_aim.is_zero_approx():
+		_mouse_recent = false
+		gun.rotation = pad_aim.angle()
+
+
+func _physics_process(_delta: float) -> void:
+	if _mouse_recent:
+		gun.rotation = get_local_mouse_position().angle()
