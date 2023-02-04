9c32fa578a11b73a5f33b87f4188cc2e361ac28e
Christopher Head <chead@chead.ca>
2023-02-04 14:21:39 -0800

    Damage damageables when touched by projectiles

--- game/projectile.gd
+++ game/projectile.gd
@@ -4,19 +4,10 @@ extends RigidBody2D
 @export_flags_2d_physics
 var delete_on_touch := 0
 
-## Whether the projectile is deleting itself
-var _deleting := false
-
-## Invoked when a projectile is about to delete itself due to touching something
-##
-## Subclasses may override this function to provide custom behaviour.
-func _crashed(_body: PhysicsBody2D) -> void:
-	pass
-
 func _on_body_entered(raw_body: Node) -> void:
-	if not _deleting:
-		var body := raw_body as PhysicsBody2D
-		if body.collision_layer & delete_on_touch:
-			_deleting = true
-			self._crashed(body)
-			get_tree().queue_delete(self)
+	var body := raw_body as PhysicsBody2D
+	var damageable := body.find_child("damageable", false) as Damageable
+	if damageable != null:
+		damageable.damage(1)
+	if body.collision_layer & delete_on_touch:
+		get_tree().queue_delete(self)
