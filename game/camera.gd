d9f6ef549474754e0305db3b04c21b94d7ec55d8
Christopher Head <chead@chead.ca>
2023-02-04 11:21:19 -0800

    Camera lookahead based on input, not velocity

--- game/camera.gd
+++ game/camera.gd
@@ -1,21 +1,8 @@
 extends Camera2D
 
-## How much to multiply the player’s velocity by when added to the camera’s target position to achieve lookahead
+## How much to multiply the player’s directional input by when added to the camera’s target position to achieve lookahead
 @export_range(0, 1000)
 var lookahead: float
 
-## The closest ancestor of type RigidBody2D
-@onready
-var rigidbody := _find_rigidbody2d_ancestor()
-
-func _find_rigidbody2d_ancestor() -> RigidBody2D:
-	var x: Node = self
-	while x != null:
-		var body := x as RigidBody2D
-		if body != null:
-			return body
-		x = x.get_parent()
-	return null
-
-func _process(_delta: float) -> void:
-	position = rigidbody.linear_velocity * lookahead
+func _unhandled_input(_event: InputEvent) -> void:
+	position = Input.get_vector("left", "right", "up", "down") * lookahead
