90f596071b469df57456b5257a2b311a33aa71aa
Christopher Head <chead@chead.ca>
2023-02-05 02:02:03 -0800

    Make rocks damage players when they land

--- game/enemy/beet/projectile.gd
+++ game/enemy/beet/projectile.gd
@@ -14,8 +14,15 @@ var target_point : Vector2
 ## The target marker, created when the projectile begins flying
 var _target_marker : Sprite2D
 
+## How much damage is dealt on impact
+var _impact_damage : float
+
 
 func _ready() -> void:
+	# This projectile is weird because it deals damage on impact, not while flying.
+	_impact_damage = damage
+	damage = 0
+
 	# This projectile is weird because it aims at a target point instead of a direction.
 	linear_velocity = linear_velocity.project(target_point - position)
 
@@ -28,6 +35,10 @@ func _ready() -> void:
 func _physics_process(_delta: float) -> void:
 	if linear_velocity.dot(target_point - position) <= 0:
 		# We’ve passed the closest point we’ll ever reach to the target, so land.
+		for i in $Area.get_overlapping_bodies():
+			var damageable : Damageable = i.get_node("Damageable")
+			if damageable != null:
+				damageable.damage(_impact_damage)
 		var rock := rock_prefab.instantiate()
 		rock.position = target_point
 		get_parent().add_child(rock)
