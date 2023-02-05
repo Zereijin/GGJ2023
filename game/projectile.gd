d25f9082bdcb02e79f36e72b66a7416ed843746a
Christopher Head <chead@chead.ca>
2023-02-05 13:16:53 -0800

    Move projectile delete determination earlier
    Corpses lose their physics layers, so if a projectile killed its target,
    it would no longer detect the collision as deletion-causing.

--- game/projectile.gd
+++ game/projectile.gd
@@ -11,7 +11,8 @@ var damage := 1
 func _on_body_entered(raw_body: Node) -> void:
 	var body := raw_body as PhysicsBody2D
 	var damageable := body.get_node("Damageable")
+	var delete := body.collision_layer & delete_on_touch
 	if damageable is Damageable:
 		damageable.damage(damage)
-	if body.collision_layer & delete_on_touch:
+	if delete:
 		get_tree().queue_delete(self)
