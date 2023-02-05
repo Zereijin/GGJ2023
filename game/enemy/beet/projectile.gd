da20ff5281045635fcb563aa7edffac8e48dbb01
Christopher Head <chead@chead.ca>
2023-02-05 02:01:28 -0800

    Add comments

--- game/enemy/beet/projectile.gd
+++ game/enemy/beet/projectile.gd
@@ -16,7 +16,10 @@ var _target_marker : Sprite2D
 
 
 func _ready() -> void:
+	# This projectile is weird because it aims at a target point instead of a direction.
 	linear_velocity = linear_velocity.project(target_point - position)
+
+	# This projectile is weird because it has a visible target marker.
 	_target_marker = target_marker_prefab.instantiate()
 	_target_marker.position = target_point
 	get_parent().add_child(_target_marker)
