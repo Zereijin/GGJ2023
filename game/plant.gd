ebb8b6fcd08c25181d15ec5e331d6b0a2ed77c46
Christopher Head <chead@chead.ca>
2023-02-04 16:29:18 -0800

    Fix facing bug

--- game/plant.gd
+++ game/plant.gd
@@ -9,13 +9,16 @@ var sprite := $Sprite
 func _process(_delta: float) -> void:
 	if constant_force.is_zero_approx():
 		sprite.stop()
-	elif constant_force.max_axis_index() == Vector2.AXIS_X:
-		if constant_force.x > 0:
-			sprite.play(&"right")
-		else:
-			sprite.play(&"left")
 	else:
-		if constant_force.y > 0:
-			sprite.play(&"down")
+		var mx := absf(constant_force.x)
+		var my := absf(constant_force.y)
+		if mx > my || is_equal_approx(mx, my):
+			if constant_force.x > 0:
+				sprite.play(&"right")
+			else:
+				sprite.play(&"left")
 		else:
-			sprite.play(&"up")
+			if constant_force.y > 0:
+				sprite.play(&"down")
+			else:
+				sprite.play(&"up")
