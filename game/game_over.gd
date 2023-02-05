06d7b67e6e7dad2a1fc9aced8a0645c99b2f33ef
Christopher Head <chead@chead.ca>
2023-02-05 11:49:21 -0800

    Don’t pause the game on game over

--- game/game_over.gd
+++ game/game_over.gd
@@ -6,13 +6,11 @@ extends Control
 @onready var scoreLabel:Label = $PanelContainer/MarginContainer/VBoxContainer/ScoreLabel
 
 func open():
-	get_tree().paused = true
 	quitButton.grab_focus()
 	scoreLabel.text = str(player.score)
 	visible = true
 
 func close():
-	get_tree().paused = false
 	quitButton.release_focus()
 	visible = false
 	var main_menu := ResourceLoader.load("res://main_menu/main_menu.tscn") as PackedScene
